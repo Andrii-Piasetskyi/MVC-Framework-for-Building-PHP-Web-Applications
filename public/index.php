031456caa9b361b362fd2aa4ce8676a898860ac9
Andrey Pyasetsky <sleivin@ukr.net>
2018-04-19 22:00:23 +0300

    router still without preg match

--- public/index.php
+++ public/index.php
@@ -1,4 +1,24 @@
 <?php
-require '../vendor/core/Router.php';
 
-$router = new Router;
+$request = $_SERVER['REQUEST_URI'];
+
+require '../vendor/core/Router.php';
+require '../vendor/libs/debugFunctions.php';
+
+Router::add('/posts/add', ['controller'=>'Posts', 'action'=>'add']);
+Router::add('/posts/', ['controller'=>'Posts', 'action'=>'index']);
+Router::add('/', ['controller'=>'Main', 'action'=>'index']);
+
+
+debug(Router::getRoutes());
+
+if(Router::matchRoute($request))
+{
+    debug(Router::getRoute());
+}
+else
+{
+    echo '404';
+}
+
+echo $request;
